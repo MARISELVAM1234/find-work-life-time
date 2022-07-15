79b6177092d2cd3eafa7f236c388e75073818271
selvam <selvamplkm111@gmail.com>
2022-07-15 16:36:42 +0530

    update-script-file.

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 //year section
 var yearSelectElement = document.getElementById("select-opt");
-yearSelectElement.setAttribute("onchange" , "displayYear(this.value)") ;
+yearSelectElement.setAttribute("onchange", "displayYear(this.value)");
 var optionElement;
-for ( var i = 1972; i <= 2006; i++) {
+for (var i = 1972; i <= 2006; i++) {
     optionElement = document.createElement("option");
     yearSelectElement.append(optionElement);
     optionElement.innerHTML = i;
-    var b = parseInt( optionElement.innerHTML)
-    console.log(typeof(b));
 }
-var valu;
-function displayYear(arugYear){
-   valu = arugYear
-   console.log(valu)
+var yearElement;
+function displayYear(arugYear) {
+    yearElement = parseInt(arugYear);
+    console.log("YEAR :", yearElement);
 }
 
 //Month section
 var monthSelectElement = document.getElementById("month-opt");
+var monthElement;
 function monthFunc() {
     var date;
-    console.log(monthSelectElement.value);
-    var x = monthSelectElement.value;
-    if (valu % 4 == 0 && monthSelectElement.value == "Feb") {
+    monthElement = parseInt(monthSelectElement.value);
+    console.log("MONTH :", monthElement);
+
+    if (yearElement % 4 == 0 && monthElement == 1) {
         date = 29;
     }
-    else if (valu % 4 != 0 && monthSelectElement.value == "Feb") {
+    else if (yearElement % 4 != 0 && monthElement == 1) {
         date = 28;
     }
-    else if (x == "Jan" || x == 'Mar' || x == "May" || x == "July" || x == "Aug" || x == "Oct" || x == "Dec"
+    else if (monthElement == 0 || monthElement == 2 || monthElement == 4 || monthElement == 6 || monthElement == 7 || monthElement == 9 || monthElement == 11
     ) {
-        date = 31 ;
+        date = 31;
     }
     else if (
-        x == "April" ||  x == "Jun" ||  x == "Sep" || x == "Nov"
+        monthElement == 3 || monthElement == 5 || monthElement == 8 || monthElement == 10
     ) {
-        date = 30 ;
+        date = 30;
     }
     else if (monthSelectElement.value == "Month") {
         alert("Please select valid month");
@@ -42,12 +42,76 @@ function monthFunc() {
 
     //date section
     var dateSelectElement = document.getElementById("date-opt");
-    dateSelectElement.innerHTML = "" ;
+    dateSelectElement.setAttribute("onchange", "displayDate(this.value)");
+
+    dateSelectElement.innerHTML = "";
     var dateSelect;
-    for (var i = 1; i <= date; i++) {
+    for (var i = 0; i <= date; i++) {
         dateSelect = document.createElement("option");
         dateSelectElement.append(dateSelect);
         dateSelect.innerText = i;
-        console.log(i);
+        // console.log(i);
     }
 }
+
+var dateElement;
+function displayDate(arugDate) {
+    dateElement = parseInt(arugDate);
+    console.log("DATE :", dateElement);
+}
+
+
+var retirementELement = document.getElementById("retrie-age");
+retirementELement.setAttribute("onchange", "displayRetire (this.value)");
+var retirement2;
+for (var i = 45; i <= 60; i++) {
+    retirement2 = document.createElement("option");
+    retirementELement.append(retirement2);
+    retirement2.innerHTML = i;
+}
+var retireAge
+function displayRetire(arugRetire) {
+    retireAge = parseInt(arugRetire);
+    console.log("RETIREMENT AGE :", retireAge);
+}
+
+
+var goElement = document.getElementById("retirement-year");
+var inputElement = document.getElementById("push-value");
+var total;
+var divElement = document.getElementById("year");
+var retirementYear;
+var d;
+var currentDate;
+var currentYear;
+function goFunc() {
+    total = yearElement + retireAge;
+    console.log("RETIREMENT YEAR :", total);
+    inputElement.value = total;
+    d = new Date(yearElement, monthElement, dateElement);
+    console.log(d)
+    currentDate = new Date();
+    currentYear = currentDate.getFullYear();
+    console.log(currentYear);
+    completeYear = currentYear - yearElement;
+    console.log(completeYear);
+    retirementYear = total - currentYear;
+    console.log(retirementYear);
+    divElement.innerHTML = "";
+    for (i = 0; i < completeYear; i++) {
+        ;
+        var createHeader2 = document.createElement("h5");
+        createHeader2.setAttribute("class", "year-box2");
+        divElement.append(createHeader2);
+    }
+    for (var i = 0; i < retirementYear; i++) {
+        var createHeader = document.createElement("h6");
+        createHeader.setAttribute("class", "year-box");
+        divElement.append(createHeader);
+    }
+
+}
+
+
+
+
